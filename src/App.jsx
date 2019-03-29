0669a517e7f051922f95161ed09240f2e43d1b11
Lukasz Grela <lukasz.grela@gmail.com>
2019-03-29 11:06:00 +0000

    App stateless component with hook routes

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useRoutes } from 'hookrouter';
+
+import Home from './pages/Home';
+import RouterCalendar from './pages/RouterCalendar';
+import LinkedCalendars from './pages/LinkedCalendars';
+import Page404 from './pages/Page404';
+
+const routes = {
+  '/': () => <Home />,
+  '/router-calendar/:year?/:month?/:date?': ({ year, month, date }) => (
+    <RouterCalendar year={year} month={month} date={date} />
+  ),
+  '/linked-calendars': () => <LinkedCalendars />,
+};
+
+const App = () => {
+  const routeResult = useRoutes(routes);
+  return (
+    //TODO: basename="/react/calendar-widget"
+    <React.Fragment>
+      <header>
+        <h1>Calendar Widget</h1>
+      </header>
+      {routeResult || <Page404 />}
+      <footer>
+        <div className="credits">
+          Arrow Up/Down by Design Effectz from the Noun Project
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+};
+
+export default App;
